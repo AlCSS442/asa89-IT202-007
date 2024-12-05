65513e86de68db0390cc2682e3b2aa75ce254d3b
AICSS442 <alanorsaleh@gmail.com>
2024-12-05 18:44:10 -0500

    added a user facing list page

--- public_html/Project/user_list.php
+++ public_html/Project/user_list.php
@@ -3,11 +3,6 @@
 
 require_once(__DIR__ . "/../../partials/nav.php");
 
-if (!has_role("Admin")) {
-    flash("You don't have permission to view this page", "warning");
-    die(header("Location: $BASE_PATH" . "/home.php"));
-}
-
 // Search parameters for filtering the list
 $cveId = se($_GET, "cveId", "", false);
 $description = se($_GET, "description", "", false);
